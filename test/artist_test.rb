a66d3b38bf5c171fac6c9f6c203204804b367c23
Mary Bork <maryebork@outlook.com>
2018-09-18 09:28:58 -0600

    Adds Iteration 1

---
+++ test/artist_test.rb
@@ -0,0 +1,84 @@
+require 'minitest/autorun'
+require 'minitest/pride'
+require './lib/artist'
+require './lib/photograph'
+
+class ArtistTest < Minitest::Test
+  def test_it_exists
+    attributes = {
+      id: "2",
+    name: "Ansel Adams",
+    born: "1902",
+    died: "1984",
+    country: "United States"
+    }
+    artist = Artist.new(attributes)
+
+    assert_instance_of Artist, artist
+  end
+
+  def test_it_has_an_id
+    attributes = {
+      id: "2",
+    name: "Ansel Adams",
+    born: "1902",
+    died: "1984",
+    country: "United States"
+    }
+    artist = Artist.new(attributes)
+
+    assert_equal "2", artist.id
+  end
+
+  def test_it_has_a_name
+    attributes = {
+      id: "2",
+    name: "Ansel Adams",
+    born: "1902",
+    died: "1984",
+    country: "United States"
+    }
+    artist = Artist.new(attributes)
+
+    assert_equal "Ansel Adams", artist.name
+  end
+
+  def test_it_has_a_year_born
+    attributes = {
+      id: "2",
+    name: "Ansel Adams",
+    born: "1902",
+    died: "1984",
+    country: "United States"
+    }
+    artist = Artist.new(attributes)
+
+    assert_equal "1902", artist.born
+  end
+
+  def test_it_has_a_year_died
+    attributes = {
+      id: "2",
+    name: "Ansel Adams",
+    born: "1902",
+    died: "1984",
+    country: "United States"
+    }
+    artist = Artist.new(attributes)
+
+    assert_equal "1984", artist.died
+  end
+
+  def test_it_has_a_country
+    attributes = {
+      id: "2",
+    name: "Ansel Adams",
+    born: "1902",
+    died: "1984",
+    country: "United States"
+    }
+    artist = Artist.new(attributes)
+
+    assert_equal "United States", artist.country
+  end
+end
